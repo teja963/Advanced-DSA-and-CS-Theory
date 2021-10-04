c8cb4925364fa684227f8a3e1a807694287d1428
teja369 <panasateja123@gmail.com>
2021-10-04 23:36:32 +0530

    top view of BT using map for storing

---
+++ Binary_Trees/38_top_view_of_BT.cpp
@@ -0,0 +1,15 @@
+
+    void topView(Node * root) {
+       map<int,Node*>m;
+       queue<pair<Node*,int>>q;
+       q.push({root,0});
+       while(!q.empty())
+       {
+           Node* x=q.front().first;
+           int p=q.front().second;
+           q.pop();
+           if(x->left)q.push({x->left,p-1});
+           if(x->right)q.push({x->right,p+1});
+           m.insert({p,x});
+       }
+       for(auto x:m)cout<<x.second->data<<" ";
