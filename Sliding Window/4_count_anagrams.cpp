789922cb79e5ae7ed78ee4dfedbbe9dfb787b63e
teja369 <panasateja123@gmail.com>
2021-10-05 16:00:28 +0530

    count anagrams use map for comparing

---
+++ Sliding Window/4_count_anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution{
+public:
+	int search(string pat, string txt) {
+	    // code here
+	    int m=txt.size(),n=pat.size();
+	    vector<int>mp(26,0),mt(26,0);
+	    for(int i=0;i<n;i++)
+	    {
+	        mt[txt[i]-'a']++;
+	        mp[pat[i]-'a']++; 
+	    }
+	    int c=0;
+	    if(mt==mp)c++;
+	    for(int i=n;i<m;i++)
+	    {
+	        mt[txt[i]-'a']++;
+	        mt[txt[i-n]-'a']--;
+	        if(mt==mp)c++;
+	    }
+	    return c;
+	}
+	    
+
