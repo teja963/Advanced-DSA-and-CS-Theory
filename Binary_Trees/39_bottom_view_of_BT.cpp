8ff7bff55f9e90cf53b670ee56c829ee6a8dc422
teja369 <panasateja123@gmail.com>
2021-10-09 21:46:36 +0530

    Bottom view of BT added

---
+++ Binary_Trees/39_bottom_view_of_BT.cpp
@@ -0,0 +1,22 @@
+lass Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+        // Your Code Here
+        vector<int>ans;
+        queue<pair<Node*,int>>q;   
+        int i;
+        map<int,int>m;
+        q.push({root,0});
+        while(!q.empty())
+        {
+            Node* x=q.front().first;
+            int p=q.front().second;
+            q.pop();
+            if(x->left)q.push({x->left,p-1});
+            if(x->right)q.push({x->right,p+1});
+            m[p]=x->data;                //taking the bottom most value in vertical condition
+        }
+        
+        for(auto x:m)ans.push_back(x.second);
+        return ans;
+    }
