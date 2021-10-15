c4fad174af3b1b0b173a69a6ee3703f7c39a3a63
teja369 <panasateja123@gmail.com>
2021-10-15 18:01:34 +0530

    Permuation all models type questions added

--- array/69_permutation_1_and_2_combined.cpp
+++ array/69_permutation_1_and_2_combined.cpp
@@ -8,3 +8,23 @@ public:
         return v;
         
     }
+    
+    
+// All unique permutations of array  O(n*n!) for both time and space
+class Solution {
+  public:
+    vector<vector<int>> uniquePerms(vector<int> arr ,int n) {
+        // code here
+      set<vector<int>>s;
+      vector<vector<int>>v;
+      sort(arr.begin(),arr.end());
+     do
+      {
+          if(s.find(arr)!=s.end())continue;
+          v.push_back(arr);
+          s.insert(arr);
+      }while(next_permutation(arr.begin(),arr.end()));
+     return v;   
+    }
+    
+};
