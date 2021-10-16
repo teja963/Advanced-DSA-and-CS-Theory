f8e7e56ef5765ee5c3dc9ee294a4a330aa21fb48
teja369 <panasateja123@gmail.com>
2021-10-16 09:45:45 +0530

    Problems overview updated

---
+++ Sliding Window/5. count distnict elements in each window.cpp
@@ -0,0 +1,19 @@
+class Solution{
+  public:
+    vector <int> countDistinct (int A[], int n, int k)
+    {
+        //code here.
+        unordered_map<int,int>m;
+        vector<int>v;
+        if(k>n)return v;
+        for(int i=0;i<k;i++)m[A[i]]++;
+        v.push_back(m.size());
+        for(int i=1;i<=n-k;i++)
+        {
+            m[A[i-1]]--;
+            if(m[A[i-1]]==0)m.erase(A[i-1]);
+            m[A[i+k-1]]++;
+            v.push_back(m.size());
+        }
+        return v;
+    }
