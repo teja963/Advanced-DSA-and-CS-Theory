259c937875efc56671c48337a99165ecdd54fefd
teja369 <panasateja123@gmail.com>
2021-10-17 14:12:02 +0530

    BST within range

---
+++ Binary_Trees/40. Print BST in given range.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+  vector<int>v;
+  void inorder(Node* root, int low, int high)
+  {
+      if(root)
+      {
+          inorder(root->left,low,high);
+          if(root->data>=low&&root->data<=high)v.push_back(root->data);
+          inorder(root->right,low,high);
+      }
+  }
+    vector<int> printNearNodes(Node *root, int low, int high) {
+        //code here  
+        inorder(root,low,high);
+        return v;
+    }
