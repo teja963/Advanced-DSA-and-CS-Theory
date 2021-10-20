2c7987e2c69195695462de11825522d144f83b74
teja369 <panasateja123@gmail.com>
2021-10-20 20:32:55 +0530

    Updated

--- Array/4. Sort 012.cpp
+++ Array/4. Sort 012.cpp
@@ -4,10 +4,7 @@ public:
         if(nums.size()==0||nums.size()==1)return;
         int l=0,m=0,h=nums.size()-1;
         while(m<=h){
-            if(nums[m]==0){
-                swap(nums[m],nums[l]);
-                l++;
-                m++;}
+            if(nums[m]==0)swap(nums[l++],nums[m++]);
             else if(nums[m]==1)m++;
             else {
                 swap(nums[m],nums[h--]);
---
+++ Array/README.md
@@ -0,0 +1,1 @@
+# Efficient Approaches
