8f4638fedb7b6994ae342186d48247d16d8780f4
teja369 <panasateja123@gmail.com>
2021-10-23 22:26:44 +0530

    BT levels in sorted order added

---
+++ Priority Queue/4. Binary tree levels in sorted order.cpp
@@ -0,0 +1,17 @@
+  vector <vector <int>> binTreeSortedLevels (int arr[], int n)
+    {
+        // Your code here
+        priority_queue<int,vector<int>,greater<int>>pq;
+        int k=log(n)/log(2),j=0;
+        vector<vector<int>>v;
+        for(int i=0;i<n;i)
+        {
+            vector<int>ans;
+            int l=0;
+            while(i<n&&l<pow(2,j)){ pq.push(arr[i++]); l++;}
+            while(pq.size()){ans.push_back(pq.top()); pq.pop();}
+            v.push_back(ans);
+            if(j<=k)j++;
+        }
+        return v;
+    }
