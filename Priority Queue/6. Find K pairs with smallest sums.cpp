5fc0745c5d613fadb9071c09b2741b95219ca5f7
teja369 <panasateja123@gmail.com>
2021-10-24 16:16:48 +0530

    K smallest pair sums added

---
+++ Priority Queue/6. Find K pairs with smallest sums.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>>ans;
+        priority_queue<pair<int,pair<int,int>>>pq;
+        for(auto x:nums1)
+        {
+            for(auto y:nums2){
+                int s=x+y;
+                if(pq.size()<k)pq.push({s,{x,y}});
+                else if(s<pq.top().first)
+                {
+                    pq.pop();
+                    pq.push({s,{x,y}});
+                }
+               else break;
+            }
+        }
+        while(pq.size())
+        {
+            ans.push_back({pq.top().second.first,pq.top().second.second});
+            pq.pop();
+        }
+        return ans;
+    }
+};
