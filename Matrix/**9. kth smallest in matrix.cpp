07cbeb7fec12aebab31d6e4f458bee8c5fb1cea0
teja369 <panasateja123@gmail.com>
2021-10-25 12:22:11 +0530

    Kth smallest in matrix added in O(nlog n)

---
+++ Matrix/**9. kth smallest in matrix.cpp
@@ -0,0 +1,24 @@
+//Updated soon
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n=matrix.size();
+        int min_element=matrix[0][0];
+        int max_element=matrix[n-1][n-1];
+        /*when we arrange all elements in order 
+        
+         min     mid           max
+          1 5 9 10 11 12 13 13 15
+          */
+        while(min_element<=max_element)
+        {
+            int mid=(min_element+max_element)/2;
+             int count=0;
+            for(int i=0;i<n;i++)
+                count+=upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
+            if(count<k)min_element=mid+1;
+            else max_element=mid-1;
+        }
+        return min_element;
+    }
+};
