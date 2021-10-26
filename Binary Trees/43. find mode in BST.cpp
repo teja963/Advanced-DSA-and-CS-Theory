b610a40ef6806c3e39e7cf15a9be7dc0f960f836
teja369 <panasateja123@gmail.com>
2021-10-27 00:12:06 +0530

    mode in BST O(n)& space O(1) by neglecting stack space

---
+++ Binary Trees/43. find mode in BST.cpp
@@ -0,0 +1,30 @@
+ TreeNode* prev=NULL;
+    int count=1,max_f=INT_MIN;
+    vector<int>v;
+    void inorder(TreeNode* root)
+    {
+        if(root)
+        {
+            inorder(root->left);
+            if(prev&&prev->val==root->val)count++;
+            else
+            {
+                count=1;
+                prev=root;
+            }
+            if(max_f<count){
+                v.clear();
+                v.push_back(root->val);
+                max_f=count;
+            }
+            else if(max_f==count)v.push_back(root->val);
+                
+            
+            inorder(root->right);
+        }
+    }
+    vector<int> findMode(TreeNode* root) {
+        inorder(root);
+        return v;
+    }
+};
