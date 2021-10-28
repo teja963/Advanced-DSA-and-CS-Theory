3361d2b477720026250f2d876dbb1d08e5ef700a
teja369 <panasateja123@gmail.com>
2021-10-28 21:51:48 +0530

    Expression tree added

---
+++ Binary Trees/45. Expression tree.cpp
@@ -0,0 +1,13 @@
+ int fun(node* root)
+    {
+        if(root)
+        {
+            int l=fun(root->left);
+            int r=fun(root->right);
+            if(!root->left&&!root->right)return stoi(root->data);
+            if(root->data=="+")return l+r;
+            if(root->data=="-")return l-r;
+            if(root->data=="*")return l*r;
+             if(root->data=="/")return l/r;
+        }
+    }
