ad606c4ceab64ef8c256ab776350184b4c72f586
teja369 <panasateja123@gmail.com>
2021-11-08 21:12:21 +0530

    Count subtree sum added

---
+++ Binary Trees/Count Number of Subtrees having given sum.cpp
@@ -0,0 +1,16 @@
+int traverse(Node* root,int &count,int X)
+{
+     if(!root)return 0;
+     int l=traverse(root->left,count,X);
+     int r=traverse(root->right,count,X);
+     if(l+r+root->data==X)count++;
+     return l+r+root->data;
+    
+}
+int countSubtreesWithSumX(Node* root, int X)
+{
+	// Code here
+	int count=0;
+	traverse(root,count,X);
+	return count;
+}
