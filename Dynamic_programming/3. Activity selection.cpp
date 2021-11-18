b2727ec67725b2f95d3dcb289c9b09070df70178
teja369 <panasateja123@gmail.com>
2021-11-18 21:28:53 +0530

    Activity selection added

---
+++ Dynamic_programming/3. Activity selection.cpp
@@ -0,0 +1,24 @@
+
+    int activitySelection(vector<int> start, vector<int> end, int n)
+    {
+        // Your code here
+        vector<pair<int,int>>v;
+        for(int i=0;i<n;i++)
+        {
+            v.push_back({end[i],start[i]});
+        }
+        
+        sort(v.begin(),v.end());
+        int count=1,j=0,i=1;
+        while(i<n)
+        {
+            if(v[i].second>v[j].first)
+            {
+                count++;
+                j=i;
+            }
+            i++;
+        }
+        return count;
+        
+    }
