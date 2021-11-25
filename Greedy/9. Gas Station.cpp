20a3052de55bdc64c2deae9cac6a354a36a2c565
teja369 <panasateja123@gmail.com>
2021-11-25 23:42:36 +0530

    gas station added

---
+++ Greedy/9. Gas Station.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int sum_gas=0,sum_cost=0,sum=0,ans=0;
+        for(int i=0;i<gas.size();i++)
+        {
+            sum_gas+=gas[i];
+            sum_cost+=cost[i];
+            sum+=gas[i]-cost[i];
+            if(sum<0)
+            {
+                ans=i+1;
+                sum=0;
+            }
+        }
+        return sum_gas<sum_cost?-1:ans;
+        
+    }
+};
