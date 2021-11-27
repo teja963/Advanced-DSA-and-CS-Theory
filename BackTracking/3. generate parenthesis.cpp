7bc6a5768032db41a86ca3534cec17a9813c4049
teja963 <panasateja123@gmail.com>
2021-11-27 11:30:55 +0530

    generate paranthesis added

---
+++ BackTracking/3. generate parenthesis.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void generate(vector<string>&ans,string tmp,int open,int close)
+    {
+        if(open==0&&close==0)
+        {
+            ans.push_back(tmp);
+            return;
+        }
+        else
+        {
+        if(open)generate(ans,tmp+'(',open-1,close+1);
+        if(close)generate(ans,tmp+')',open,close-1);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        string tmp;
+        generate(ans,tmp,n,0);
+        return ans;
+    }
+};
