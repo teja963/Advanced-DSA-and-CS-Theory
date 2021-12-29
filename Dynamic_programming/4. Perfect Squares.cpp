62b08c28eb3b8b4703a1fcb87f623d33783f72d9
teja963 <panasateja123@gmail.com>
2021-12-29 12:38:46 +0530

    Mins coins and perfect squares added

--- Dynamic_programming/4. Perfect Squares.cpp
+++ Dynamic_programming/4. Perfect Squares.cpp
@@ -15,3 +15,26 @@ public:
         return v[n];
     }
 };
+
+
+//Min No.of coins
+int minCoins(int coins[], int M, int V) 
+	{ 
+	    // Your code goes here
+	    //calculate(coins,M,V);
+	    vector<int>dp(V+1,1e9);
+	    dp[0] = 0;
+	    for(int i = 1;i <= V; i++)
+	    {
+	        for(int j = 0; j <M; j++)
+	        {
+	            if(i >= coins[j])
+	            {
+	              dp[i] = min(dp[i], 1+dp[i-coins[j]]);
+	            }
+	        }
+	    }
+	    return dp[V] == 1e9? -1: dp[V];
+	}
+	  
+};
