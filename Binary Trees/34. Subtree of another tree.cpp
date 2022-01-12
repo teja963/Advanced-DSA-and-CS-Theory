b5de1f3a53a25ce72e9a463f0bb8d7234b0a491f
teja963 <panasateja123@gmail.com>
2022-01-12 12:20:02 +0530

    Updated

--- Binary Trees/34. Subtree of another tree.cpp
+++ Binary Trees/34. Subtree of another tree.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     bool check(TreeNode* root1,TreeNode* root2)       //check both trees are same or not function
     {
-        if(!root1&&!root2)return true;
-        if(!root1||!root2)return false;
-        if(root1->val!=root2->val)return false;
-        return check(root1->left,root2->left)&&check(root1->right,root2->right);
+        if(!root1 && !root2)return true;
+        if(!root1 || !root2)return false;
+        if(root1->val != root2->val)return false;
+        return check(root1->left,root2->left) && check(root1->right,root2->right);
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(!root&&!subRoot)return true;
-        if(!root||!subRoot)return false;
-        if(root->val==subRoot->val)                   //if node values of both trees matches verify same or not
+        if(!root && !subRoot)return true;
+        if(!root || !subRoot)return false;
+        if(root->val == subRoot->val)                   //if node values of both trees matches verify same or not
         {
             if(check(root,subRoot))return true;
         }
-        if(isSubtree(root->left,subRoot)||isSubtree(root->right,subRoot))  //verify each node recursively
-		return true;
-        return false;
+        return (isSubtree(root->left,subRoot)||isSubtree(root->right,subRoot))  //verify each node recursivel
     }
--- Dynamic_programming/8_subset_equal_partition.cpp
+++ Dynamic_programming/8_subset_equal_partition.cpp
@@ -1,43 +1,27 @@
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
- // } Driver Code Ends
-// User function Template for C++
-int subset(int arr[],int n,int s){
-    if(s==0)return 1;
-    if(n==0||s<0)return 0;
-    else return subset(arr,n-1,s-arr[n-1])||subset(arr,n-1,s);
-}
-class Solution{
-public:
-    int equalPartition(int N, int arr[])
+ int equalPartition(int N, int arr[])
     {
         // code here
-        int i,j,k,s=0;
-        for(i=0;i<N;i++)s+=arr[i];
-        if(s%2!=0)return 0;
-        else return subset(arr,N,s/2);
-    }
-};
-
-// { Driver Code Starts.
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int N;
-        cin>>N;
-        int arr[N];
-        for(int i = 0;i < N;i++)
-            cin>>arr[i];
+        int sum = 0;
+        for(int i = 0; i < N; i++)sum += arr[i];
+        if(sum % 2 != 0)return 0;
+        sum /= 2;
+        vector<vector<int>>dp(N+1,vector<int>(sum+1,0));
+        for(int i = 0; i <= N; i++)dp[i][0] = 1;
+        for(int i = 1; i <= N; i++)
+        {
+            for(int j = 1; j <= sum; j++)
+            {
+                if(j - arr[i-1] >= 0)
+                {
+                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[N][sum];
         
-        Solution ob;
-        if(ob.equalPartition(N, arr))
-            cout<<"YES\n";
-        else
-            cout<<"NO\n";
+        
     }
-    return 0;
