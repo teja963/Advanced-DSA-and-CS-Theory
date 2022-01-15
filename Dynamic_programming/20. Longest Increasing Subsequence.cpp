d56655ad1ad2c5ad62a86f16ad9e56cdbc55d2b3
teja963 <panasateja123@gmail.com>
2022-01-15 15:39:38 +0530

    Longest Increasing Subsequence added

---
+++ Dynamic_programming/20. Longest Increasing Subsequence.cpp
@@ -0,0 +1,18 @@
+  int longestSubsequence(int n, int a[])
+    {
+       // your code here
+       int dp[n];
+       dp[0] = 1;
+       for(int i = 1; i < n; i++)
+       {
+           dp[i] = 1;
+           for(int j = 0; j < i; j++)
+           {
+               if(a[j] < a[i])
+               {
+                   dp[i] = max(dp[i], dp[j] + 1);
+               }
+           }
+       }
+       return *max_element(dp,dp + n);
+    }
