97cae03f144524113378858c58c93520620db9bd
teja963 <panasateja123@gmail.com>
2022-02-01 12:14:48 +0530

    check

--- Array/11. Best time to buy sell stock.cpp
+++ Array/11. Best time to buy sell stock.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-         int i,j,p=pow(2,20),m=0,n;
-        n=prices.size();
-         for(i=0;i<n;i++){
-             if(prices[i]<p)p=prices[i];
-             else if(prices[i]-p>m)m=prices[i]-p;
+         int i,p = pow(2,20),m = 0, n;
+        n = prices.size();
+         for(i = 0; i < n; i++){
+             if(prices[i] < p)p=prices[i];
+             else if(prices[i]-p > m)m = prices[i]-p;
          }
         return m;
     }
