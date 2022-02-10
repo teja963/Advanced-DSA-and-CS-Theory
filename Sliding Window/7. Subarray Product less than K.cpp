7ee2dbce42e28f6c097f842e0ef4b82ac88f0096
teja963 <panasateja123@gmail.com>
2022-02-10 13:50:32 +0530

    Subarray Product less than K

---
+++ Sliding Window/7. Subarray Product less than K.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+     if(k <= 1)return 0;
+      int left = 0, right, ans = 0;
+     long long prod = 1; 
+     for(right = 0; right < nums.size(); right++)
+      {
+          prod *= nums[right];
+          while(left <= right && prod >= k)prod /= nums[left++];
+          ans += (right - left + 1);
+      }
+      return ans;
+    }
+};
