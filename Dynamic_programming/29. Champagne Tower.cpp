bbd990f565bded69292ab9e9ccbd819520eaf36a
teja963 <panasateja123@gmail.com>
2022-03-05 01:14:12 +0530

    champagne tower

---
+++ Dynamic_programming/29. Champagne Tower.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        double dp[101][101] = {0.0};
+        dp[0][0] = poured;
+        for(int i = 0; i < 100; i++){
+            for(int j = 0; j <= i; j++){
+                if(dp[i][j] >= 1){
+                    dp[i+1][j] += (dp[i][j] - 1) / 2.0;   //left
+                    dp[i+1][j+1] += (dp[i][j] - 1) / 2.0;
+                    dp[i][j] = 1;
+                }
+            }
+        }
+        return dp[query_row][query_glass];
+    }
+};
