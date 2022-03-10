29977466f4af6e18def739fc3849908424c1e0f6
teja963 <panasateja123@gmail.com>
2022-03-10 17:08:16 +0530

    Add two numbers added

--- Linked list/9. Add two nums LL.cpp
+++ Linked list/9. Add two nums LL.cpp
@@ -1,30 +1,31 @@
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* t=l1,*tmp=NULL,*x,*j=l1;
-        while(j->next)j=j->next;
-        int c=0,p,k=0;
-        while(l1&&l2){
-            p=l1->val+l2->val+c;
-            l1->val=p%10;
-            c=p/10;
-            l1=l1->next;
-            l2=l2->next;
+        int carry = 0;
+        ListNode* ans = NULL, *travel = NULL;
+        while(l1 || l2 || carry){
+            int sum = 0;
+            if(l1){
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if(l2){
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            ListNode* tmp = new ListNode(sum % 10);
+            if(!ans){
+                ans = tmp;
+                travel = tmp;
+            }
+            else{
+                travel->next = tmp;
+                travel = travel->next;
+            }
         }
-        if(c>0&&!l1&&!l2)j->next=new ListNode(c);
-        else if(l1){
-            while(l1){ p=l1->val+c; l1->val=p%10; c=p/10; l1=l1->next; k=1;}
-           if(c>0&&k==1)j->next=new ListNode(c);
-           }
-        else{
-            tmp=new ListNode(0);
-            x=tmp;
-            k=0;
-            while(l2){ p=l2->val+c; x->next=new ListNode(p%10); c=p/10; l2=l2->next; x=x->next; k=1;}
-            if(c>0&&k==1)x->next=new ListNode(c);
-            j->next=tmp->next;
-          }
-        return t;
-        
+        return ans;
     }
 };
