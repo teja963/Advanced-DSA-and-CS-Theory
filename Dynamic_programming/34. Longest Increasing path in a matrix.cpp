4c80c1f808f9ed3eaa0110b31a5548b56f463d24
teja963 <panasateja123@gmail.com>
2022-05-30 10:05:04 +0530

    Both dfs and bfs added

--- Dynamic_programming/34. Longest Increasing path in a matrix.cpp
+++ Dynamic_programming/34. Longest Increasing path in a matrix.cpp
@@ -1,3 +1,4 @@
+/*DFS Approach*/
 class Solution {
 public:
     int dp[201][201];
@@ -23,5 +24,37 @@ public:
             }
         }
         return ans;
+    }
+};
+
+/**BFS APPROACH**/
+    vector<pair<int ,int>>p = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    int bfs(int curr_i, int curr_j, vector<vector<int>>&m, vector<vector<int>>&dp){
+        if(dp[curr_i][curr_j])return dp[curr_i][curr_j];
+        int ans = 1;
+            for(int i = 0; i < 4; i++){
+                int new_i = curr_i + p[i].first;
+                int new_j = curr_j + p[i].second;
+                if(new_i >= 0 and new_j >= 0 and new_i < m.size() and new_j < m[0].size()){
+                    if(m[new_i][new_j] < m[curr_i][curr_j]){
+                        ans = max(ans, 1 + bfs(new_i, new_j, m, dp));
+                    }
+                }
+            }
+        return dp[curr_i][curr_j] = ans;
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        // Code here
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int ans = 1;
+        vector<vector<int>>dp(n, vector<int>(m, 0));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                ans = max(ans, bfs(i, j, matrix, dp));
+            }
+        }
+        return ans;
+        
     }
 };
