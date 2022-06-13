0710ec47bbc6054e407d4d5ee9300c70e945b834
teja963 <panasateja123@gmail.com>
2022-06-13 13:19:35 +0530

    Triangle

---
+++ Dynamic_programming/37. Triangle.cpp
@@ -0,0 +1,17 @@
+https://github.com/teja963/Advanced-DSA
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int>v(n);
+        for(int i = 0; i < triangle[n-1].size(); i++){
+            v[i] = triangle[n-1][i];
+        }
+        for(int i = n-2; i >= 0; i--){
+            for(int j = 0; j < triangle[i].size(); j++){
+                v[j] = triangle[i][j] + min(v[j], v[j+1]);
+            }
+        }
+        return v[0];
+    }
+};
