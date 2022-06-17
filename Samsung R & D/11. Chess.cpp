0aad294196d3189bd6db5183475c537b7cd455b0
teja963 <panasateja123@gmail.com>
2022-06-17 16:15:35 +0530

    chess description added

--- Samsung R & D/11. Chess.cpp
+++ Samsung R & D/11. Chess.cpp
@@ -1,3 +1,33 @@
+/*
+There is a mobile piece and a stationary piece on the N×M chessboard. 
+The available moves of the mobile piece are the same as set out in the image below. 
+You need to capture the stationary piece by moving the mobile piece with the minimum amount of moves.
+Write a program to find out the minimum number moves to catch a piece. 
+[Input]
+Several test cases can be included in the inputs. T, the number of cases is given in the first row of the
+inputs. After that, the test cases as many as T (T ≤ 20) are given in a row. 
+N, the numbers of the rows and M, the number of columns of the chessboard are given in the first row of
+each test case. 
+R & C is the location information of the attacking piece and S & K is the location of the defending pieces 
+and are given in the row at the second line. However, the location of the uppermost end of the left end 
+is (1, 1)
+[Output]
+For each test case, you should print "Case #T" in the first line where T means the case number.
+For each test case, you should output the minimum number of movements to catch a defending piece at the 
+first line of each test case. If not moveable, output equals ‘-1’. 
+[I/O Example]
+Input 
+2
+9 9
+3 5 2 8
+20 20
+2 3 7 9
+Output
+Case #1
+2
+Case #2
+5 
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int t, n, m, ans;
