8773ec476efb49aee91cd89e7504a6111d325106
teja963 <panasateja123@gmail.com>
2022-06-18 17:20:28 +0530

    Fisher Man description added

--- Samsung R & D/19. Fisher Man.cpp
+++ Samsung R & D/19. Fisher Man.cpp
@@ -1,3 +1,35 @@
+/*
+There are N fishing spots and 3 gates. At each gate there are some fishermen waiting to reach the nearest unoccupied fishing spot. (Total no of fisherman <=N)
+
+Distance between consecutive spots = distance between gate and nearest spot = 1 m
+
+Only 1 gate can be opened(other gates after that gate) at a time and all fishermen of that gate must occupy the spots before the next gate is opened.
+
+Distance is calculated as gate to nearest spot + nearest spot to closest vacant spot. Find the total sum of minimum distances need to walk for all the fishermen.
+
+image
+
+Input Format
+
+Number of fishing spots, N Position of the gates, Gi, 1 < i < 3 Number of fishermen at each gates, G_n_i
+
+Constraints
+
+1 <= N <= 10 1 <= G_i <= N 1 <= G_n_i <= 15
+
+Output Format
+
+Output single integer
+
+Sample Input 0
+
+10
+4 6 10
+5 2 2
+Sample Output 0
+
+18
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int p1, p2, p3, n1, n2, n3, n, ans;
