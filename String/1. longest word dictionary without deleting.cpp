71793a93e9f03ce969b00283d917f0970c1af8ae
teja963 <panasateja123@gmail.com>
2022-07-22 14:31:52 +0530

    Updated

--- String/1. longest word dictionary without deleting.cpp
+++ String/1. longest word dictionary without deleting.cpp
@@ -1,29 +1,46 @@
-//Input: s = "abpcplea", dictionary =["ale","apple","monkey","plea"]
-//Output: "apple"
+/*
+Given a string s and a string array dictionary, return the longest string in the dictionary that can be formed by deleting
+some of the given string characters. If there is more than one possible result, return the longest word with the smallest
+lexicographical order. If there is no possible result, return the empty string.
 
-//Input: s = "abpcplea", dictionary = ["a","b","c"]
-//Output: "a"
+
+Example 1:
+
+Input: s = "abpcplea", dictionary = ["ale","apple","monkey","plea"]
+Output: "apple"
+Example 2:
+
+Input: s = "abpcplea", dictionary = ["a","b","c"]
+Output: "a"
+ 
+
+Constraints:
+1 <= s.length <= 1000
+1 <= dictionary.length <= 1000
+1 <= dictionary[i].length <= 1000
+s and dictionary[i] consist of lowercase English letters
+*/
 class Solution {
 public:
     static bool cmp(string &a,string &b){
-        if(a.size()==b.size())return a<b;
-        return a.size()>b.size();
+        if(a.size() == b.size())return a < b;
+        return a.size() > b.size();
     }
     
-    bool contain(string &s,string &a){ 
-        if(s.size()<a.size())return false;
-        int i=0;
+    bool contain(string &s, string &a){ 
+        if(s.size() < a.size())return false;
+        int i = 0;
         for(auto x:s){
-            if(i<a.size()&&a[i]==x)i++;
+            if(i < a.size() && a[i] == x)i++;
         }
-        if(i==a.size())return true;
+        if(i == a.size())return true;
         return false;
     }
     string findLongestWord(string s, vector<string>& dictionary) {
-       sort(dictionary.begin(),dictionary.end(),cmp);
-       int i=0;
-       for(i=0;i<dictionary.size();i++){
+       sort(dictionary.begin(), dictionary.end(), cmp);
+       int i = 0;
+       for(i = 0; i < dictionary.size(); i++){
            if(contain(s,dictionary[i]))return dictionary[i];
        }
-        return "";
+       return "";
     }
--- String/2. longest substring without repeating  char.cpp
+++ String/2. longest substring without repeating  char.cpp
@@ -1,27 +1,28 @@
-//Input: s = "abcabcbb"
-//Output: 3
-//Explanation: The answer is "abc", with the length of 3.
+/*
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
 
-//Input: s = "bbbbb"
-//Output: 1
-//Explanation: The answer is "b", with the length of 1.
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
 
-//Input: s = "pwwkew"
-//Output: 3
-//Explanation: The answer is "wke", with the length of 3. Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
-//Input: s = ""
-//Output: 0
+Input: s = ""
+Output: 0
+*/
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         unordered_set<char>set;  //for storing unique char using sliding window
-        int i=0,j=0,size=0;  //two pointer method
-        while(i<s.size()&&j<s.size()){
-            if(set.find(s[j])==set.end()){
+        int i = 0,j = 0,size = 0;  //two pointer method
+        while(i < s.size() && j < s.size()){
+            if(set.find(s[j]) == set.end()){
                 set.insert(s[j]);
-                int n=set.size();
-                size=max(size,n);
+                size = max(size, set.size());
                 j++;
             }
             else{
@@ -30,22 +31,21 @@ public:
             }
         }
         return size;
-             
     }
     
     
-// 2nd method
+// 2nd method critical thinking
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-      vector<int>v(256,-1);
-        int i,j=0,k=0,x;
-        for(i=0;i<s.size();i++){
-            j=max(j,v[s[i]]+1); 
-            x=i-j+1;  // maintaining the len size of string
-            k=max(k,x); //final string len as ans
-            v[s[i]]=i;
+      vector<int>v(256, -1);
+        int i, j = 0,k = 0, x;
+        for(i = 0;i < s.size(); i++){
+            j = max(j, v[s[i]]+1); 
+            x = i-j+1;  // maintaining the len size of string
+            k = max(k,x); //final string len as ans
+            v[s[i]] = i;
         }
         return k;
     }
