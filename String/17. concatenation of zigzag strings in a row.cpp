dd3f541dd7382a64f64702a98f25de96e6d19d6f
teja963 <panasateja123@gmail.com>
2022-07-22 16:22:09 +0530

    Updated

--- String/17. concatenation of zigzag strings in a row.cpp
+++ String/17. concatenation of zigzag strings in a row.cpp
@@ -2,22 +2,22 @@ class Solution{
     public:
     string convert(string s, int n) {
         //code
-        if(n<=1)return s;      //base condition
+        if(n <= 1)return s;      //base condition
         vector<string>v(n);
         string ans,tmp;
-        int count=0;
+        int count = 0;
         for(int i=0;i<s.size();i++)
         {
-            v[count]+=s[i];
-            if(count==0)tmp="down";
-            if(count==n-1)tmp="up";
-            if(tmp=="down")
+            v[count] += s[i];
+            if(count == 0)tmp = "down";
+            if(count == n-1)tmp = "up";
+            if(tmp == "down")
             {
                 count++;
             }
             else count--;
         }
-        for(auto x:v)ans+=x;
+        for(auto x:v)ans += x;
         return ans;
         
     }
--- String/20. Group Anagrams.cpp
+++ String/20. Group Anagrams.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
        vector<vector<string>>ans;
-        unordered_map<string,vector<string>>m;
-        for(auto x:strs)
+       unordered_map<string,vector<string>>m;
+        for(auto x : strs)
         {
-         string s=x;
-         sort(s.begin(),s.end());
+         string s = x;
+         sort(s.begin(), s.end());
          m[s].push_back(x);
         }
-        for(auto x:m)
+        for(auto x : m)
         {
             ans.push_back(x.second);
         }
