430fef22810b7948d0042bd6153f94fa47652f0c
teja963 <panasateja123@gmail.com>
2022-09-07 10:57:06 +0530

    stack permutations

---
+++ Stack/2. Stack Permutations.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int isStackPermutation(int n, int[] ip, int[] op) {
+        // code here
+        Stack<Integer>st = new Stack<Integer>();
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            st.push(ip[i]);
+            if(st.peek() == op[j]){
+                while(st.size() > 0 && st.peek() == op[j]){
+                        st.pop();
+                        j++;
+                }
+            }
+        }
+        return st.empty()?1:0;
+    }
+}
