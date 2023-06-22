047c0daf40d7b46679f59f823e82bcb4da89678b
teja963 <panasateja123@gmail.com>
2023-06-22 11:02:45 +0530

    added

--- Array/33. Subarray with equal 0s and 1s.cpp
+++ Array/33. Subarray with equal 0s and 1s.cpp
@@ -1,18 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-
- // } Driver Code Ends
-
-
 class Solution{
   public:
     //Function to count subarrays with 1s and 0s.
     long long int countSubarrWithEqualZeroAndOne(int arr[], int n)
     {
         //Your code here
-        long long int c=0;
-        int i,j,s=0;
+        long long int c = 0;
+        int i, j ,s = 0;
         unordered_map<int, int>m;
         for(i = 0; i < n; i++){
             if(arr[i] == 0)arr[i] = -1;
@@ -25,21 +20,3 @@ class Solution{
     }
 };
 
-// { Driver Code Starts.
-
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	int n,i;
-	cin>>n;
-	int arr[n];
-	for(i=0;i<n;i++)
-	cin>>arr[i];
-	Solution obj;
-	cout<< obj.countSubarrWithEqualZeroAndOne(arr, n)<<"\n";
-	}
-	return 0;
-}
