2ce048d6d506795fb1341a573ff806dcda6943db
teja963 <panasateja123@gmail.com>
2023-06-22 16:45:40 +0530

    Readme added

---
+++ Searching Sorting/Easy ques/1. Find 1st and last position in sorted array.cpp
@@ -0,0 +1,38 @@
+/*
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+*/
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        pair<vector<int>::iterator,vector<int>::iterator>x;
+        x = equal_range(nums.begin(), nums.end(), target);
+        if(x.first == nums.end() || *x.first != target)return {-1,-1}; // if x is not present or not found
+        return {int(x.first-nums.begin()),int(x.second-nums.begin()-1)};
+    }
