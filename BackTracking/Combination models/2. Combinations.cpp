1fac1c3a2401841bf2886da7311e5992d756a756
teja963 <panasateja123@gmail.com>
2023-06-22 16:11:11 +0530

    Back tracking filtered

--- BackTracking/Combination models/2. Combinations.cpp
+++ BackTracking/Combination models/2. Combinations.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     vector<int>tmp,numbers;
     vector<vector<int>>ans;
-    void findcombi(int start,int k,int n)
+    void findcombi(int start, int k, int n)
     {
-        if(tmp.size()==k)
+        if(tmp.size() == k)
         {
             ans.push_back(tmp);
             return ;
         }
-        for(int i=start;i<n;i++)
+        for(int i = start; i < n; i++)
         {
             tmp.push_back(numbers[i]);
-            findcombi(i+1,k,n);
+            findcombi(i+1, k, n);
             tmp.pop_back();
         }
     }
     vector<vector<int>> combine(int n, int k) {
-        for(int i=0;i<n;i++)numbers.push_back(i+1);
-        findcombi(0,k,n);
+        for(int i = 0; i < n; i++)numbers.push_back(i+1);
+        findcombi(0, k, n);
         return ans;
     }
 };
